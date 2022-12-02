b47baa581bef444aa788b0df3c64acd5618ee594
francinepalero <107932534+francinepalero@users.noreply.github.com>
2022-12-02 09:54:16 +0800

    Layout

--- Mendiola_Palero_BT701_TP/app/src/main/java/com/example/mendiola_palero_bt701_tp/MainActivity.java
+++ Mendiola_Palero_BT701_TP/app/src/main/java/com/example/mendiola_palero_bt701_tp/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.mendiola_palero_bt701_tp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
