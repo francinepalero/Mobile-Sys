562bc8da5d7967747999cbcff2ee965441f6331c
francinepalero <107932534+francinepalero@users.noreply.github.com>
2023-01-13 09:40:35 +0800

    push

--- MendiolaPalero_FinalsTP/app/src/main/java/com/example/mendiola_palero_finalstp/MainActivity.java
+++ MendiolaPalero_FinalsTP/app/src/main/java/com/example/mendiola_palero_finalstp/MainActivity.java
@@ -3,34 +3,46 @@ package com.example.mendiola_palero_finalstp;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 
     // NUMBER 3
-    ImageView logo;
-    //Button button;
+    private ImageView logo;
+    private Button button;
+    private Spinner spinner;
 
     // NUMBER 5
-    private final String sharedPrefFile = "com.example.mendiola_palero_finalstp";
-    SharedPreferences mPreferences = getSharedPreferences(sharedPrefFile, MODE_PRIVATE);
+    private String sharedPrefFile = "com.example.mendiola_palero_finalstp";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //button = (Button)findViewById(R.id.button1);
-        // NUMBER 3
-        logo.setImageResource(R.drawable.school_logo);
 
-        //button.setOnClickListener(view -> {
+        spinner = (Spinner) findViewById(R.id.sp1);
 
-        // NUMBER 5
-        int x = 100;
-        String y = String.valueOf(x);
-        SharedPreferences.Editor editor = mPreferences.edit();
-        editor.putInt("max", x);
-        editor.putString("max", y);
-        editor.apply();
+        button = (Button) findViewById(R.id.button1);
+
+        SharedPreferences mPreferences = getSharedPreferences(sharedPrefFile, MODE_PRIVATE);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = spinner.getSelectedItem().toString();
+
+
+                // NUMBER 5
+                int x = 100;
+                String y = String.valueOf(x);
+                SharedPreferences.Editor editor = mPreferences.edit();
+                editor.putInt("max", x);
+                editor.apply();
+            }
+        });
     }
 }
